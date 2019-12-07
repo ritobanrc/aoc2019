6ac47b7b0db31546ebc0ae40be9a54aaf861d77e
Ritoban Roy-Chowdhury <ritobanrc@gmail.com>
2019-12-07 10:51:16 -0800

    second test works, adding vscode debugging

--- src/day7.rs
+++ src/day7.rs
@@ -220,17 +220,18 @@ fn solve_p2(tape: &Vec<i32>) -> i32 {
         let mut phases_passed = vec![false; seq.len()];
         'feedback: loop {
             for (i, phase) in seq.iter().enumerate() {
-                let state = intcode_computer_p2(&mut tapes[i], &mut ips[i], || { 
-                    if phases_passed[i] {
-                        output
-                    } else {
+                let state = intcode_computer_p2(&mut tapes[i], &mut ips[i], || {
+                    if !phases_passed[i] {
                         phases_passed[i] = true;
+                        //println!("Phase passed for {:?}: {:?}", i, phase);
                         *phase
+                    } else {
+                        output
                     }
                 });
 
 
-                println!("{:?}", output);
+                //println!("{:?}", state);
 
                 if state == -1 {
                     break 'feedback
@@ -239,7 +240,6 @@ fn solve_p2(tape: &Vec<i32>) -> i32 {
                 output = state;
             }
         }
-
         if output > max_signal {
             max_signal = output;
         }
@@ -321,7 +321,7 @@ fn intcode_computer_p2<F>(tape: &mut Vec<i32>, i: &mut usize, mut get_input: F) 
                 *i += 4;
             }
             [0, 3] => {
-                let output = tape[*i + 3] as usize;
+                let output = tape[*i + 1] as usize;
                 tape[output] = get_input();
                 *i += 2;
             }
@@ -436,71 +436,18 @@ mod tests {
     fn day7_sample1() {
         let tape = vec![3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5];
 
-        let seq = vec![9, 8, 7, 6, 5];
-
-        let mut output = 0;
-        let mut tapes = vec![tape.clone(); seq.len()];
-        let mut ips = vec![0; seq.len()];
-        let mut phases_passed = vec![false; seq.len()];
-        'feedback: loop {
-            for (i, phase) in seq.iter().enumerate() {
-                let state = intcode_computer_p2(&mut tapes[i], &mut ips[i], || { 
-                    if phases_passed[i] {
-                        output
-                    } else {
-                        phases_passed[i] = true;
-                        *phase
-                    }
-                });
-
-
-                println!("{:?}", output);
-
-                if state == -1 {
-                    break 'feedback
-                }
-
-                output = state;
-            }
-        }
+        let output = solve_p2(&tape);
         assert_eq!(output, 139629729);
-
-        println!("Seq: {:?} Gives {:?}", seq, output);
     }
 
 
     #[test]
     fn day7_sample2() {
         let tape = vec![3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54, -5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4, 53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10];
-        let seq = vec![9, 7, 8, 5, 6];
+        //let seq = vec![9, 7, 8, 5, 6];
 
-        let mut output = 0;
-        let mut tapes = vec![tape.clone(); seq.len()];
-        let mut ips = vec![0; seq.len()];
-        let mut phases_passed = vec![false; seq.len()];
-        'feedback: loop {
-            for (i, phase) in seq.iter().enumerate() {
-                let state = intcode_computer_p2(&mut tapes[i], &mut ips[i], || {
-                    if phases_passed[i] {
-                        output
-                    } else {
-                        phases_passed[i] = true;
-                        *phase
-                    }
-                });
-
-
-                println!("{:?}", state);
-
-                if state == -1 {
-                    break 'feedback
-                }
-
-                output = state;
-            }
-        }
+        let output = solve_p2(&tape);
         assert_eq!(output, 18216);
 
-        println!("Seq: {:?} Gives {:?}", seq, output);
     }
 }
